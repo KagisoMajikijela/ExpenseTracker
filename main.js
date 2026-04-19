7737d378f887d4d4780678b984940a82fe2230f1
kagiso majikijela <majikijelaclinton@gmail.com>
2026-04-19 22:37:06 +0200

    Added user input fields

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+const btnaddexpense=document.getElementById('btn-add-expense').addEventListener('click',addexpense)
 
+function addexpense(){
+        const txtname=document.getElementById('txt-name').value
+        const txtprice=document.getElementById('txt-price').value
+        const filtercmb=document.getElementById('filter-cmb').value
+
+    try {
+            const Newp=document.createElement('p').textContent="Input field should have values"
+            if(txtname===""||txtprice==="")
+                document.body.append(Newp)
+            else{
+                const Newli=document.createElement('li')
+                Newli.textContent=txtname
+                Newli.classList.add('.items')
+                console.log(Newli)
+        }
+    } catch (er) {
+        console.er('something went wrong')
+    }
+
+}
