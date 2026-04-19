b0974750a42cb7d1fef791e94b2d1c662c87a31d
kagiso majikijela <majikijelaclinton@gmail.com>
2026-04-19 23:29:43 +0200

    added dynamic division styled feature

--- main.js
+++ main.js
@@ -4,19 +4,40 @@ function addexpense(){
         const txtname=document.getElementById('txt-name').value
         const txtprice=document.getElementById('txt-price').value
         const filtercmb=document.getElementById('filter-cmb').value
-
-    try {
-            const Newp=document.createElement('p').textContent="Input field should have values"
-            if(txtname===""||txtprice==="")
-                document.body.append(Newp)
-            else{
-                const Newli=document.createElement('li')
-                Newli.textContent=txtname
-                Newli.classList.add('.items')
-                console.log(Newli)
+        const price =txtprice
+        console.log(price)
+        const Newp=document.createElement('p').textContent="Input field should have values"
+        if(txtname===""||txtprice==="")
+            document.body.append(Newp)
+        else{
+            const Newli=document.createElement('li')
+            Newli.textContent=txtname
+            Newli.classList.add('.display')
+            document.body.appendChild(Newli)
+            console.log(Newli)
         }
-    } catch (er) {
-        console.er('something went wrong')
+
+        const Newdiv=document.createElement('div')
+        const Newul =document.createElement('ul')
+        const Newli =document.createElement('li')
+
+        Newli.textContent=txtname
+
+        Newul.classList.add('items')
+        Newul.append(Newli)
+        Newdiv.appendChild(Newul)
+        Newdiv.classList.add('display')
+
+
+        document.body.append(Newdiv)
+        console.log(Newli)
+        console.log(Newul)
+        console.log(Newdiv)
+
+        const Divtotal=document.querySelector('.calc-container')
+        const h2total=document.querySelector('h2-total')
+
+        console.log(h2total)
+        console.log(Divtotal)
     }
 
-}
