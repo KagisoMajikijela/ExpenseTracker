0835e1337611600d582bddc5bba40b90251e0442
kagiso majikijela <majikijelaclinton@gmail.com>
2026-04-21 17:09:12 +0200

    fixed code dry

--- main.js
+++ main.js
@@ -1,23 +1,18 @@
 const btnaddexpense=document.getElementById('btn-add-expense')
 btnaddexpense.addEventListener('click',GetInput)
+
 function GetInput(){
+    
     const txtname=document.getElementById('txt-name').value
     const txtprice=document.getElementById('txt-price').value
     const txtdescription=document.getElementById('txt-description')
+    const cmbfilter=document.getElementById('cmb-filter')
    
     //array to store User inputs
     let expenses=[
-        {name:""},
-        {price:""},
-        {description:""}
-    ]
-    expenses.push(name,txtname)
-    // expenses.push(name.txtname)
-    // expenses.push(price.txtprice)
-    // expenses.push(description.txtdescription)
-    // Nexpenses.push(name,NewExpense)
-    const NewExpense=[...expenses]
-    NewExpense.push([name,"NewExpsne track"])
+    ];
+    
+    expenses.push({name:txtname,price:txtprice,description:txtdescription})
     console.log(expenses)
-    console.log(NewExpense)
+   alert(expenses)
 }
