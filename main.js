88ba0be1f3b3a0ebcbb762aebe0602b4d5c28898
kagiso majikijela <majikijelaclinton@gmail.com>
2026-04-23 17:13:46 +0200

    fixing the getTotal function

--- main.js
+++ main.js
@@ -1,32 +1,43 @@
 
 const btnaddexpense=document.getElementById('btn-add-expense')
 btnaddexpense.addEventListener('click',GetInput)
-let expenses=[
-    ];
- let Total;
 
-function getTotal(){
-    const h2counter=document.querySelector('.h2-total')
-    h2counter.textContent=`Total: R${Total}`
+let expenses=[];
+
+let Total=0
+//function to get and update the Total
+ function getTotal(){
+    const amount=expenses.forEach(el=>{
+        Total+el.price 
+        return Total
+    })
+    console.log(amount)
     console.log(Total)
-    //expenses.forEach(amount=>{
-    //Total+=amount.price
-    //})
+
+    return Total
 }
 
+
+//function to get the user inputs
 function GetInput(){
     //getting user inputs
     const txtname=document.getElementById('txt-name').value
     const txtprice=document.getElementById('txt-price').value
     const txtdescription=document.getElementById('txt-description').value
     const cmbfilter=document.getElementById('cmb-filter')
+    Total=txtprice
 
     //array to store User inputs
-    expenses.push({name:txtname,price:txtprice,description:txtdescription})
+    expenses.push({name:txtname,price:Number(txtprice),description:txtdescription})
+
     // console.log(expenses)
+
+    //Calling outer funtions
     display()
     getTotal()
 }
+
+
 //functon to display expenses inside the array of objects
 function display(){
   const ul=document.querySelector('.items')
@@ -44,14 +55,3 @@ function display(){
   })
 }
 
-//function to calculate total later after fixing element display 
-
-// // getTotals()
-//     function getTotals(amount){
-//         
-//         // let amount
-//         // Total+=amount
-//         
-//       // return amount
-//       console.log(h2counter)
-//     }
